b7ab491c13b377dcbeea71a29b85d366ab16bb09
chenyongze <sapphire.php@gmail.com>
2018-07-04 15:30:25 +0800

    shop

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+/*
+import Home from '../pages/home'
+*/
+import Detail from '../pages/goodsDetail'
+import Msg from '../components/message'
+
+
+Vue.use(VueRouter)
+const routes = [
+  {
+    path: '/',
+    component: resolve => require(['../pages/home'],resolve),
+    meta: {
+      title:'home'
+    }
+  },
+  {
+    path: '/msg',
+    component: Msg
+  },
+  {
+    path: '/detail',
+    component: Detail,
+    children: [
+      {
+        path:'msg',
+        component: Msg
+      }
+    ]
+  }
+]
+export default new VueRouter({
+    mode: 'history',
+    routes
+})
